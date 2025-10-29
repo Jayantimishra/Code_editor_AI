6016a1295114f91838c057c6071523a8661a6e64
Jayantimishra <mishrajayanti956@gmail.com>
2025-10-29 16:35:30 +0530

    latest commit

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -2,20 +2,29 @@
 
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import TemplateFileTree from "@/features/playground/components/template-file-tree";
+import { usePlayground } from "@/features/playground/hooks/usePlayground";
 import { useParams } from "next/navigation";
 import React from "react";
 
 const Page =() => {
     const {id} =  useParams<{id:string}>();
+    const {playgroundData, templateData, isLoading, error, saveTemplateData} = usePlayground(id);
+
+    console.log(templateData, playgroundData);
     return (
         <div>
-            <>  
+            <> 
+            <TemplateFileTree />
+
             <SidebarInset>
-                <header className="px-4 py-2 border-b border-gray-200 dark:border-gray-700">
+                <header className="px-4 gap-2 h-16 flex shrink-0 items-center border-b">
                     <SidebarTrigger>
                         <Separator orientation="vertical" className="my-2" />
                         <div className="flex items-center space-x-2">
-                            <div className="flex flex-col flex-1"></div>
+                            <div className="flex flex-col flex-1">
+                                {playgroundData?.title || "Code Playground"}
+                            </div>
                         </div>
 
                     </SidebarTrigger>
